5042d1b8c109f17539d7abd6e307ada8c1c8dbde
sethwololo <sethwololo@gmail.com>
2020-04-22 19:09:56 -0300

    Atualizando avatar e servindo arquivos estáticos

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
-import express from 'express';
 import 'reflect-metadata';
 
+import express from 'express';
 import routes from './routes';
+import uploadConfig from './config/upload';
 
 import './database';
 
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 // eslint-disable-next-line no-console
