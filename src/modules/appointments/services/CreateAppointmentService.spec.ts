a452a4af89d0b77d06945d57c0a9e98c5f48af64
Matheus Henrique Moura Bezerra <sethwololo@Mac-mini-de-Matheus.local>
2020-06-08 14:20:54 -0300

    Testes de appointment e create user

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,4 +1,5 @@
 import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
+import AppError from '@shared/errors/AppError';
 import CreateAppointmentService from './CreateAppointmentService';
 
 describe('CreateAppointment', () => {
@@ -17,7 +18,24 @@ describe('CreateAppointment', () => {
     expect(appointment.provider_id).toBe('idteste123');
   });
 
-  // it('should not be able to create two appointments in the same hour', () => {
-  //   expect(1 + 2).toBe(3);
-  // });
+  it('should not be able to create two appointments in the same hour', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointmentDate = new Date(2020, 4, 10, 11);
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: 'idteste123',
+    });
+
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: 'idteste123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
