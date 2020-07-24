b4a8f349488e4e18aa22ab35b63ea1bc76eeaaeb
sethwololo <sethwololo@gmail.com>
2020-07-24 16:06:50 -0300

    Utilizando Class Transformer

--- src/modules/appointments/infra/http/controllers/ProvidersController.ts
+++ src/modules/appointments/infra/http/controllers/ProvidersController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import ListProvidersService from '@modules/appointments/services/ListProvidersService';
 
@@ -9,10 +10,10 @@ export default class ProvidersController {
 
     const listProviders = container.resolve(ListProvidersService);
 
-    const appointment = await listProviders.execute({
+    const providers = await listProviders.execute({
       user_id,
     });
 
-    return response.json(appointment);
+    return response.json(classToClass(providers));
   }
 }
